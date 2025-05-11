397e89ed41dada5f5c3b6da0883fa23d4f9b9257
Sfundo-cfc <149521391+Sfundo-cfc@users.noreply.github.com>
2025-05-11 20:08:39 +0200

    untracked overrides and carousel files added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const path = require("path");
 const pageRoutes = require("./routes/pageRoutes");
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
